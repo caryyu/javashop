23fb8ad99d240db779f68b0776f1778c7d02a4a1
cary <liang.yu@nakedhub.cn>
2017-07-15 14:52:45 +0800

    测试类编写

---
+++ src/test/java/com/github/caryyu/JavaSdkRelatedTest.java
@@ -0,0 +1,13 @@
+package com.github.caryyu;
+
+import org.junit.Test;
+
+/**
+ * Created by cary on 7/15/17.
+ */
+public class JavaSdkRelatedTest {
+    @Test
+    public void test1(){
+        System.out.println("Hello World");
+    }
+}
